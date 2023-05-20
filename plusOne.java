5d83e4deb57b6d0df6d4bc7bfec85ce021cec9be
avenky72 <109006663+avenky72@users.noreply.github.com>
2023-05-19 21:36:09 -0700

    Update plusOne.java

--- plusOne.java
+++ plusOne.java
@@ -1,5 +1,26 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        int j = digits.length;
+        for (int i = j - 1; i >= 0; i--)
+            {if (digits[i] < 9){
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+            else {
+                digits[i] = 0;}
+        }
+        digits = new int [digits.length + 1];
+        digits[0] = 1;
+        return digits;
+
+    }
+}
+
+/* 
+FAILED ATTEMPTS
+
+class Solution {
+    public int[] plusOne(int[] digits) {
         int i = digits.length;
         int num = 0;
         if (digits[i-1] < 9){
@@ -15,3 +36,4 @@ class Solution {
 
     }
 }
+*/
