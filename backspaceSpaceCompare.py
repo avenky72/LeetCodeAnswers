19989431313b6ac785723c070928d25614309092
Atul Venkatesan <109006663+avenky72@users.noreply.github.com>
2024-03-13 23:11:19 -0700

    Update backspaceSpaceCompare.py

--- backspaceSpaceCompare.py
+++ backspaceSpaceCompare.py
@@ -1,4 +1,5 @@
 class Solution(object):
+# Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
     def backspaceCompare(self, s, t):
         """
         :type s: str
