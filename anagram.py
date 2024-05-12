6e1ee34cb1b12585a355874c769d7b944cea012a
Atul Venkatesan <109006663+avenky72@users.noreply.github.com>
2024-05-12 12:54:35 -0700

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,13 @@
+# Check if two strings are anagrams
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s = sorted(s)
+        t = sorted(t)
+
+        return t == s
