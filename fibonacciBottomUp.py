fbcbbe98907bbc121f6978197d40a695b79a0635
Atul Venkatesan <109006663+avenky72@users.noreply.github.com>
2024-07-21 19:16:19 -0700

    Create fibonacciBottomUp.py

---
+++ fibonacciBottomUp.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        table = [0, 1]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        else:
+            for i in range(2, n+1):
+                temp = table[-1] + table[-2]
+                table.append(temp)
+                table.pop(0)
+            return table[-1]
