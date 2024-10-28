5b04a09d708abb9ba2dec264ae628f071bf96530
Atul Venkatesan <109006663+avenky72@users.noreply.github.com>
2024-10-27 22:56:43 -0700

    Create kthLargestSumBinaryTree.py

---
+++ kthLargestSumBinaryTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        q = deque([root])
+        sums = []
+        while q:
+            level_sum = 0
+            for i in range(len(q)):
+                node = q.popleft()
+                level_sum += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            sums.append(level_sum)
+        sums.sort() 
+        return sums[-k] if k <= len(sums) else -1 
